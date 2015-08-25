9dceea42bbb9dda29b767a57ce5c08b72bfc6f15
Cynthia Hanna <cynthiahanna11@gmail.com>
2015-08-25 13:37:59 -0400

    minimum viable product

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,49 @@
 // jshint devel:true
 console.log('\'Allo \'Allo!');
+
+$(function() {
+
+  var mark = 'X';
+
+  $('[id^=cell]').click(function() {
+    var $cell = $(this);
+    $cell.text(mark);
+
+
+    if (mark === 'X') {
+      mark = 'O';
+    } else {
+      mark = 'X';
+    }
+
+    checkForVictory();
+    $cell.off('click');
+
+  });
+});
+
+var lines = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9],
+  [1,4,7],
+  [2,5,8],
+  [3,6,9],
+  [1,5,9],
+  [3,5,7]
+];
+
+function checkForVictory() {
+  lines.forEach(function(line) {
+    var $cell1 = $('#cell-' + line[0]),
+        $cell2 = $('#cell-' + line[1]),
+        $cell3 = $('#cell-' + line[2]);
+
+    if ($cell1.text() === '') return;
+
+    if ($cell1.text() === $cell2.text() &&
+        $cell2.text() === $cell3.text()) {
+      return alert($cell1.text() + ' is the winner!');
+    }
+  });
+ }
